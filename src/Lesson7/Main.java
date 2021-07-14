476c2252a880e60ac93efb7039cc7d15c193b094
TMichael <js5859168@gmail.com>
2021-07-14 13:28:14 +0300

    Lesson #7 All tasks v. 1.01 with boolean

--- src/Lesson7/Main.java
+++ src/Lesson7/Main.java
@@ -9,11 +9,11 @@ public class Main {
         Cat[] cats = new Cat[5];//для задания 4 создаем массив котов
 
 
-        cats[0] = new Cat("Барсик", 0);
-        cats[1] = new Cat("Кузя", 0);
-        cats[2] = new Cat("Сеня", 0);
-        cats[3] = new Cat("Муся", 0);
-        cats[4] = new Cat("Люся", 0);
+        cats[0] = new Cat("Барсик", false, 100 );
+        cats[1] = new Cat("Кузя", false, 100 );
+        cats[2] = new Cat("Сеня", false, 100 );
+        cats[3] = new Cat("Муся", false, 100);
+        cats[4] = new Cat("Люся", false, 100);
 
         Plate plate = new Plate(450, 100);
 
